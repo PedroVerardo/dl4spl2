2c24a548263e47f1162051aefa916e6cd2a31cf4
João Marcello Bessa <jm.bessa09@gmail.com>
2025-01-23 17:28:57 -0300

    Adição dos arquivos de feature selection

---
+++ grid_search.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from utils import LightningModel
+import lightning as L
+import os
+import torch
+
+def select_top_features(df, top_percentage=0.7):
+    df_sorted = df.sort_values(by='importance', ascending=False)
+    top_n = int(len(df_sorted) * top_percentage)
+    top_features = df_sorted.head(top_n)['features'].tolist()
+    return df[top_features]
+
+if __name__ == '__main__':
+    decision_tree = pd.read_csv('feature_importance_DT.csv')
+    random_forest = pd.read_csv('feature_importance_RF.csv')
+    gradient_boosting = pd.read_csv('feature_importance_GB.csv')
+
+    top_percentage=0.7
+
+    top_decision_tree = select_top_features(decision_tree, top_percentage)
+    top_random_forest = select_top_features(random_forest, top_percentage)
+    top_gradient_boosting = select_top_features(gradient_boosting, top_percentage)
+
+    data = pd.read_parquet('data.parquet')
+    selected_data = data[top_decision_tree]
+
+    LightningModel(num_features=len(selected_data.columns), activation="PReLU", optimizer_name="Adam", loss_name="MSELoss")
+
+    model = LightningModel(num_features=len(selected_data.columns), activation="PReLU", optimizer_name="Adam", loss_name="MSELoss")
+
+    train_size = int(0.8 * len(selected_data))
+    test_size = len(selected_data) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(selected_data, [train_size, test_size])
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1024, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1024, shuffle=False)
+
+    trainer = L.Trainer(
+        max_epochs=10,
+        accelerator='auto',
+        devices=1,
+        enable_progress_bar=True
+    )
+
+    checkpoint_path = "checkpoints/best-checkpoint.ckpt"
+    if os.path.exists(checkpoint_path):
+        trainer.fit(model, train_loader, test_loader, ckpt_path=checkpoint_path)
+    else:
+        trainer.fit(model, train_loader, test_loader)
+
---
+++ utils.py
@@ -0,0 +1,72 @@
+import lightning as L
+import torch
+from torch import nn
+
+class LightningModel(L.LightningModule):
+    def __init__(self, num_features, activation="ReLU", optimizer_name="Adam", loss_name="MSELoss"):
+        super().__init__()
+        self.num_features = num_features
+        self.activation = activation
+        self.optimizer_name = optimizer_name
+        self.loss_name = loss_name
+        self.model = self.build_model()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, _ = batch
+        x = x.view(x.size(0), -1)
+        z = self(x)
+        loss = self.get_loss_function()(z, x)
+        self.log('train_loss', loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, _ = batch
+        x = x.view(x.size(0), -1)
+        z = self(x)
+        loss = self.get_loss_function()(z, x)
+        self.log('val_loss', loss)
+        return loss
+
+    def configure_optimizers(self):
+        return self.get_optimizer()
+
+    def get_optimizer(self):
+        optimizers = {
+            "Adam": torch.optim.Adam(self.parameters(), lr=0.001),
+            "SGD": torch.optim.SGD(self.parameters(), lr=0.001),
+            "RMSprop": torch.optim.RMSprop(self.parameters(), lr=0.001)
+        }
+        return optimizers[self.optimizer_name]
+
+    def get_loss_function(self):
+        loss_functions = {
+            "MSELoss": nn.MSELoss(),
+            "L1Loss": nn.L1Loss(),
+            "SmoothL1Loss": nn.SmoothL1Loss(),
+            "CrossEntropyLoss": nn.CrossEntropyLoss()
+        }
+        return loss_functions[self.loss_name]
+
+    def get_activation(self):
+        activations = {
+            "ReLU": nn.ReLU(),
+            "PReLU": nn.PReLU(),
+            "ELU": nn.ELU()
+        }
+        return activations[self.activation]
+
+    def build_model(self):
+        hidden_size = self.num_features // 2
+        hidden_size2 = hidden_size // 2
+        return nn.Sequential(
+            nn.Linear(self.num_features, hidden_size),
+            self.get_activation(),
+            nn.Dropout(0.3),
+            nn.Linear(hidden_size, hidden_size2),
+            self.get_activation(),
+            nn.Dropout(0.3),
+            nn.Linear(hidden_size2, 1)
+        )
